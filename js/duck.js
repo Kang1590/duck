93a738392f711a44867c821065ca88d97a105f13
Kang1590 <139293643+Kang1590@users.noreply.github.com>
2023-08-25 11:39:54 +0900

    Add files via upload

---
+++ js/duck.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+// 첫화면 fade슬라이드
+  const slide = setInterval( fade , 2000 );
+  let i = 3;
+  function fade(){
+    i--; //[2,1,0]
+    if(i == 0){
+      $("#index main img").fadeIn( 1000 );
+      i = 3;
+    }else{
+      $("#index main img").eq(i).fadeOut( 1000 );
+    }
+  }
+});
